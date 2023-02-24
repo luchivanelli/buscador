8f322563b45e6167bb5ca48128fd408a7a997666
Luciana <lucianavanelli13gmail.com>
2023-02-23 21:08:22 -0300

    arreglos

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,20 @@
 import './content.css'
 
 const Content = ({photos, open})=> {
-    return (
-        <div className="container">
-            <div className="container-content">
-                {photos.map(photo => 
-                    <article key={photo.id} onClick={()=> open(photo.links.html)} className='content-article'>
-                    <img src={photo.urls.regular}></img>
-                    <p>{[photo.description, photo.alt_description].join(' - ')}</p>
-                    </article>
-                )}
+    if (photos.length !== 0) {
+        return (
+            <div className="container">
+                <div className="container-content">
+                    {photos.map(photo => 
+                        <article key={photo.id} onClick={()=> open(photo.links.html)} className='content-article'>
+                        <img src={photo.urls.regular}></img>
+                        <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+                        </article>
+                    )}
+                </div>
             </div>
-        </div>
-    )
+        )
+    } 
 }
 
 export default Content
